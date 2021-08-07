03203ffaa6fb27a013922fc99ab2ba5419a9597c
kssytsrk <kassy@dismail.de>
2021-08-07 13:32:24 +0300

    implemented decryption and encryption

--- main.hs
+++ main.hs
@@ -5,23 +5,24 @@ import Control.Monad
 import System.IO
 import System.Exit
 import System.Environment
-import Data.Text
-import qualified Data.Text.IO as T
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
+import Data.Char
 
 data Options = Options  { optCommand    :: String
                         , optVerbose    :: Bool
-                        , optInput      :: IO Text
-                        , optOutput     :: Text -> IO ()
-                        , optPad        :: IO Text
+                        , optInput      :: IO T.Text
+                        , optOutput     :: T.Text -> IO ()
+                        , optPad        :: (IO T.Text, T.Text -> IO ())
                         , optLength     :: Int
                         }
 
 startOptions :: Options
 startOptions = Options  { optCommand    = "decrypt"
                         , optVerbose    = False
-                        , optInput      = T.getContents
-                        , optOutput     = T.putStr
-                        , optPad        = T.getContents
+                        , optInput      = TI.getContents
+                        , optOutput     = TI.putStr
+                        , optPad        = (TI.getContents, TI.putStr)
                         , optLength     = 0
                         }
 
@@ -41,24 +42,25 @@ options =
         "Generate a one-time pad"
     , Option "i" ["input"]
         (ReqArg
-            (\arg opt -> return opt { optInput = T.readFile arg })
+            (\arg opt -> return opt { optInput = TI.readFile arg })
             "FILE")
         "Input file"
     , Option "o" ["output"]
         (ReqArg
-            (\arg opt -> return opt { optOutput = T.writeFile arg })
+            (\arg opt -> return opt { optOutput = TI.writeFile arg })
             "FILE")
-        "Output file"
+        "Output file (for generation, decryption, and encryption)"
     , Option "p" ["pad"]
         (ReqArg
-            (\arg opt -> return opt { optPad = T.readFile arg })
+            (\arg opt -> return opt { optPad = (TI.readFile arg,
+                                                TI.writeFile arg) })
             "FILE")
-        "One-time pad to use"
+        "One-time pad to use (for decryption and encryption)"
     , Option "l" ["length"]
         (ReqArg
             (\arg opt -> return opt { optLength = read arg :: Int })
             "INT")
-        "New one-time pad's length"
+        "New one-time pad's length (for generation)"
     , Option "v" ["verbose"]
         (NoArg
             (\opt -> return opt { optVerbose = True }))
@@ -81,39 +83,38 @@ options =
 main :: IO ()
 main = do
   args <- getArgs
-  -- Parse options, getting a list of option actions
   let (actions, nonOptions, errors) = getOpt RequireOrder options args
-
-  -- Here we thread startOptions through all supplied option actions
   opts <- Prelude.foldl (>>=) (return startOptions) actions
-
   let Options { optCommand = command
               , optVerbose = verbose
               , optInput   = input
               , optOutput  = output
-              , optPad     = pad
-              , optLength  = length} = opts
+              , optPad     = (inPad, outPad)
+              , optLength  = length } = opts
 
   case command of
     "generate" -> generate output length
     "encrypt"  -> do
       inputContents <- input
-      padContents <- pad
+      padContents <- inPad
       output $ encrypt inputContents padContents
     "decrypt"  -> do
       inputContents <- input
-      padContents <- pad
+      padContents <- inPad
       output $ decrypt inputContents padContents
 
-generate :: (Text -> IO ()) -> Int -> IO ()
+generate :: (T.Text -> IO ()) -> Int -> IO ()
 generate output length = do
   withBinaryFile "/dev/random" ReadMode
     (\handle -> do
         contents <- replicateM length $ hGetChar handle
-        output $ pack contents)
+        output $ T.pack contents)
 
-encrypt :: Text -> Text -> Text
-encrypt = error "implement encryption, please!"
+crypt :: (Int -> Int -> Int) -> T.Text -> T.Text -> T.Text
+crypt f = T.zipWith (\s1 s2 -> chr (f (ord s1) (ord s2)))
 
-decrypt :: Text -> Text -> Text
-decrypt = error "implement decryption, please!"
+encrypt :: T.Text -> T.Text -> T.Text
+encrypt = crypt (+)
+
+decrypt :: T.Text -> T.Text -> T.Text
+decrypt = crypt (-)
